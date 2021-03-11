ea602f454082312a815e2beaf190aac2998b48a5
Alice Piar <adpir30@gmail.com>
2021-03-11 00:40:40 -0600

    adding navbar and background css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Background from "./components/Background";
+import './index.css';
 
+// function Greeting() {
+//   return (
+//     <div>
+//       <Question />
+//       <Tutorial />
+//     </div>
+
+//   );
+
+
+// }
+// const Question = () => <h1>What are you practice?</h1>
+// const Tutorial = () => {
+//   return (
+//     <p> Learning React</p>
+
+//   )
+// }
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <section className="App">
+            <Navbar />
+            <Background />
+        </section>
+    )
 }
+
+// const Book = () => {
+//     return (
+//         <section>
+//             <article className="book">
+//                 <h1 >
+//                     Outlander
+//     </h1>
+//                 <img src="https://m.media-amazon.com/images/I/51PrW27sXWL.jpg" alt='' />
+//                 <h4>Outlander: A Novel (Outlander, Book 2) </h4>
+//                 <h3>Book 5 of 8: </h3>
+//                 <a href="#">Outlander</a>
+//             </article>
+
+//         </section>
+//     )
+// }
 
 export default App;
