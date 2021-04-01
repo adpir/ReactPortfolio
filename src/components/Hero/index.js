aaf656bed58887648b61d82fea4669012f61174f
Alice Piar <adpir30@gmail.com>
2021-03-31 19:23:42 -0500

    add animations

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-undef */
 import React from 'react'
 import Videos from '../../Videos/Abstract.mp4';
+import Fade from 'react-reveal/Fade'
 
 // import Header from '../Header'
 import Footer from '../Footer'
@@ -19,16 +20,18 @@ const Hero = ({
 
 
                 <div
-                    className=" relative flex items-center justify-center h-3/4
+                    className=" relative flex items-center justify-center h-full
                 bg-cover bg-center bg-fixed bg-no-repeat"
 
                     style={{
                         backgroundImage: `url(${Videos})`
                     }}>
-                    {heading && <h1 className="
-                 relative px-2.5 text-gray-400 font-mono uppercase z-10 text-4xl text-center italic md:text-4xl  opacity-50">
-                        {heading}
-                    </h1>}
+                    <Fade left cascade>
+                        {heading && <h1 className="
+                 relative px-2.5 text-gray-400 font-mono uppercase z-10 text-4xl text-center italic md:text-4xl ">
+                            {heading}
+                        </h1>}
+                    </Fade>
                     <div className="
                     z-0 absolute -top-0 -left-0 h-full w-full bg-black opacity-40
                     "
